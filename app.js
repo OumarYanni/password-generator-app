2f933ffc004b1d826a0c4988bea40ec6627cf091
Oumar Yanni <oumaryanni@gmail.com>
2024-01-24 21:11:26 +0000

    Javascript logic for password strength management

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const characterLengthInput = document.querySelector("#character-length-slider");
 const sliderValueElement = document.querySelector("#slider-value");
 
 async function passwordCopyHandling() {
+  /*onclick="passwordCopyHandling()" */
   try {
     await navigator.clipboard.writeText(
       document.querySelector("#display-password").value
@@ -10,12 +11,17 @@ async function passwordCopyHandling() {
 
     console.log("Texte copié dans le presse-papier !");
 
-    //return true;
+    return true;
   } catch (err) {
     console.error("Echec de la copie :", err);
 
-    //return false;
+    return false;
   }
+}
+
+function handleCopyButton() {
+  const copyButton = document.querySelector(".material-symbols-outlined");
+  copyButton.addEventListener("click", passwordCopyHandling);
 }
 
 /*async function handleCopyButtonClick() {
@@ -32,29 +38,15 @@ function updateDisplay(elem, val) {
 }
 
 function updateSliderDisplay() {
+  /*oninput="updateSliderDisplay(this.value)" */
+
   updateDisplay(sliderValueElement, characterLengthInput.value);
 
   console.log("La longueur des caractères est :", characterLengthInput.value);
 }
 
-function updateStrengthIndicator() {
-  const password = document.querySelector("#display-password").value;
-  const indicator = document.querySelector("#password-strength-indicator");
-  const strength = getPasswordStrength(password);
-
-  indicator.className = "indicator";
-
-  indicator.classList.add(strength);
-
-  //   if (strength === "too-weak") {
-  //     indicator.classList.add("too-weak");
-  //   } else if (strength === "weak") {
-  //     indicator.classList.add("weak");
-  //   } else if (strength === "medium") {
-  //     indicator.classList.add("medium");
-  //   } else if (strength === "strong") {
-  //     indicator.classList.add("strong");
-  //   }
+function handleSliderInput() {
+  characterLengthInput.addEventListener("input", updateSliderDisplay);
 }
 
 function getPasswordStrength(password) {
@@ -70,3 +62,45 @@ function getPasswordStrength(password) {
     return "too-weak";
   }
 }
+
+function updateStrengthIndicator() {
+  const password = document.querySelector("#display-password").value;
+  const indicator = document.querySelector("#password-strength-indicator");
+  const strengthText = document.querySelector(".password-strength-text");
+  const strength = getPasswordStrength(password);
+
+  indicator.className = "indicator";
+  indicator.classList.add(strength);
+
+  switch (strength) {
+    case "too-weak":
+      strengthText.textContent = "TOO WEAK!";
+      break;
+
+    case "weak":
+      strengthText.textContent = "WEAK";
+      break;
+
+    case "medium":
+      strengthText.textContent = "MEDIUM";
+      break;
+
+    case "strong":
+      strengthText.textContent = "STRONG";
+      break;
+  }
+}
+
+function handlePasswordInput() {
+  const passwordInput = document.querySelector("#display-password");
+
+  passwordInput.addEventListener("input", updateStrengthIndicator);
+}
+
+function main() {
+  handleSliderInput();
+  handleCopyButton();
+  handlePasswordInput();
+}
+
+main();
